50fc4d836ed19ec1ed1b7ed742003c8bac2fecaf
ern4m <ernam.rsf@gmail.com>
2025-02-14 20:39:56 -0300

    fix: Alteração da rota "/" da blueprint Dashboard + geração do template de libraries com dados reais

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,37 +1,23 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
-from .models import db, User, Lending, Book
+from .models import db, User, Lending, Book, Library
 
 dashboard = Blueprint('dashboard', __name__)
 
 @dashboard.route('/')
 @login_required
 def main():
-    user = User.query.get(current_user.id)
-
-    libraries = user.libraries
-
-    borrowings = Lending.query.filter_by(borrower_id=user.id).all()
-    lendings = Lending.query.filter_by(lender_id=user.id).all()
-
-    new_books = Book.query.filter(Book.library_id.in_([library.id for library in libraries]),
-                                   Book.lending_id == None,  # Not lent
-                                   Book.is_lent == False,    # Not lent
-                                   Book.id > 0).all()
-    return render_template('dashboard/dashboard.html',
-                           libraries=libraries,
-                           borrowings=borrowings,
-                           lendings=lendings,
-                           new_books=new_books,
-                           username=user.username)
-    # return f"Hello, {user.lent_books}! Welcome to your dashboard."
+    return redirect(url_for('dashboard.libraries'))
 
 @dashboard.route('/libraries')
 @login_required
 def libraries():
     user = User.query.get(current_user.id)
 
-    return render_template('dashboard/libraries.html', username=user.username)
+    libraries = Library.query.filter_by(user_id=user.id).all()
+    print(libraries, user.id)
+
+    return render_template('dashboard/libraries.html', libraries=libraries, username=user.username)
 
 @dashboard.route('/borrowings')
 @login_required

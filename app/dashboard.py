48caf90225adcabce06395dda0c4fd0ba0a674a5
ern4m <ernam.rsf@gmail.com>
2025-02-27 14:58:59 -0300

    fix: Removing placeholders on others libraries listing

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, session
 from flask_login import login_required, current_user
 from .models import db, User, Lending, Book, Library
 
@@ -14,9 +14,18 @@ def main():
 def libraries():
     user = User.query.get(current_user.id)
 
+    others_libraries = Library.query\
+                            .filter(Library.user_id != user.id)\
+                            .order_by(Library.created_at.desc())\
+                            .limit(10)\
+                            .all()
+
     libraries = Library.query.filter_by(user_id=user.id).all()
 
-    return render_template('dashboard/libraries.html', libraries=libraries, username=user.username)
+    return render_template('dashboard/libraries.html',
+                            libraries=libraries,
+                            username=user.username,
+                            others_libraries=others_libraries)
 
 @dashboard.route('/borrowings')
 @login_required
